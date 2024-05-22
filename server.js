50065fb355dc13cd273ca8a676e00429a42d1765
kayejaye87 <166473066+kayejaye87@users.noreply.github.com>
2024-05-22 03:28:01 +0000

    6

--- server.js
+++ server.js
@@ -19,6 +19,57 @@ app.use(express.urlencoded({extended: true}));
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient();
 
+// This example uses Express to receive webhooks
+const express = require('express')
+const app = express()
+
+// Instantiating formsg-sdk without parameters default to using the package's
+// production public signing key.
+const formsg = require('@opengovsg/formsg-sdk')()
+
+// This is where your domain is hosted, and should match
+// the URI supplied to FormSG in the form dashboard
+const POST_URI = 'https://plumber.gov.sg/webhooks/9d2440b0-6b13-4810-9fec-e1f2e8dea25a'
+
+// Your form's secret key downloaded from FormSG upon form creation
+const formSecretKey = process.env.FORM_SECRET_KEY
+
+// Set to true if you need to download and decrypt attachments from submissions
+const HAS_ATTACHMENTS = true
+
+app.post(
+  '/submissions',
+  // Endpoint authentication by verifying signatures
+  function (req, res, next) {
+    try {
+      formsg.webhooks.authenticate(req.get('X-FormSG-Signature'), POST_URI)
+      // Continue processing the POST body
+      return next()
+    } catch (e) {
+      return res.status(401).send({ message: 'Unauthorized' })
+    }
+  },
+  // Parse JSON from raw request body
+  express.json(),
+  // Decrypt the submission
+  async function (req, res, next) {
+    // If `verifiedContent` is provided in `req.body.data`, the return object
+    // will include a verified key.
+    const submission = HAS_ATTACHMENTS
+      ? await formsg.crypto.decryptWithAttachments(formSecretKey, req.body.data)
+      : formsg.crypto.decrypt(formSecretKey, req.body.data)
+
+    // If the decryption failed, submission will be `null`.
+    if (submission) {
+      // Continue processing the submission
+    } else {
+      // Could not decrypt the submission
+    }
+  }
+)
+
+app.listen(8080, () => console.log('Running on port 8080'))
+
 // Main landing page
 app.get('/', async function(req, res) {
 
